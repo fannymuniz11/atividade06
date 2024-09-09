205cddcc8f3f9efb798f6d04daaa190e94ff9307
fannymuniz11 <munizfanny03@gmail.com>
2024-09-09 09:08:04 -0300

    atividade 06

---
+++ att 06.py
@@ -0,0 +1,6 @@
+# Crie um programa que receba dois nomes (primeiro nome e sobrenome) e exiba o nome completo.
+
+Nm1 = input("seu primeiro nome aqui")
+Nm2 = input("seu sobrenome aqui")
+
+print (F"seu nome completo é{Nm1}{Nm2}")
